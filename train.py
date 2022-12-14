bdd07b51e976a4cbd3ac5e8cdeabd2210ca688d1
QuangDuc-HUST <ducquangng.03@gmail.com>
2022-12-14 03:13:02 +0000

    build data_loader and model

--- train.py
+++ train.py
@@ -1,3 +1,26 @@
 #
 # main file 
 # 
+
+import torch
+
+from utils import seed_everything
+
+
+def get_training_device():
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("We are training on ", device)
+    return device
+
+if __name__ == '__main__':
+
+    # set everything
+    seed_everything(seed=73)
+
+    # get training device
+    device = get_training_device()
+
+
+
+
+
--- utils.py
+++ utils.py
@@ -1,4 +1,10 @@
+import os
+import random
+
 import subprocess
+
+import numpy as np
+import torch 
 
 
 def runcmd(cmd, is_wait=False, *args, **kwargs):
@@ -11,3 +17,11 @@ def runcmd(cmd, is_wait=False, *args, **kwargs):
     
     if is_wait:
         process.wait()
+    
+def seed_everything(seed=73):
+    random.seed(seed)
+    os.environ["PYTHONHASHSEED"] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
