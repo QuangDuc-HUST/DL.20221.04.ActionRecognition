7078fa5d2e20fd804e406892c59ed08585a3a207
ladailam382 <ladailam382@gmail.com>
2022-12-22 08:48:55 +0700

    add backend model

---
+++ app.py
@@ -0,0 +1,42 @@
+# Importing Necessary modules
+from fastapi import FastAPI, File, UploadFile
+from tempfile import NamedTemporaryFile
+from pydantic import BaseModel
+from deployment.model import *
+import os
+import traceback
+
+
+
+# Declaring our FastAPI instance
+app = FastAPI()
+
+class request_body(BaseModel):
+    description: str
+    
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile, model_name: str):
+
+    temp = NamedTemporaryFile(delete=False)
+    try:
+        print('Reading video..')
+        try:
+            contents = file.file.read()
+            with temp as f:
+                f.write(contents);
+        except Exception as e:
+            print(traceback.format_exc())
+            print("There was an error uploading the file")
+        finally:
+            file.file.close()
+
+        res = predict(temp.name, file.filename, model_name)
+    except Exception as e:
+        print(traceback.format_exc())
+        print("There was an error processing the file")
+    finally:
+        print("Reading completed")
+        os.remove(temp.name)
+
+    return {"filename": res}
+
