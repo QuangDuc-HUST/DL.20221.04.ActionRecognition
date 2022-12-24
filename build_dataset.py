70e65a15b1a49547172e557857e172cb8d2ee152
QuangDuc-HUST <ducquangng.03@gmail.com>
2022-12-24 15:09:05 +0700

    Update readme.md

--- build_dataset.py
+++ build_dataset.py
@@ -19,7 +19,8 @@ def get_dataset_arg():
     parser.add_argument('--google', action="store_true")
     parser.add_argument('--google_access_key', type=str, default='')
     parser.add_argument('--dataset', required=True, type=str, choices=['ucf101', 'hmdb51'])
-    parser.add_argument('--process_type', type=str, default='5_frames_uniform', choices=['5_frames_uniform', '16_frames_conse_rand'] )
+    parser.add_argument('--process_type', type=str, default='5_frames_uniform', 
+                        choices=['5_frames_uniform', '16_frames_conse_rand', '5_clips_16_frames'] )
     
     return parser.parse_args()
 
@@ -50,6 +51,9 @@ def get_data_kaggle_id(dataset, process_type):
 
 if __name__ == '__main__':
     args = get_dataset_arg()
+
+    if args.process_type == '5_clips_16_frames':
+        raise("Big file !Please using Google Colab version and download in google colab instead.")
 
     if (args.google and args.kaggle) or (not args.kaggle and not args.google):
         raise("Please choose one google or kaggle for download.")
